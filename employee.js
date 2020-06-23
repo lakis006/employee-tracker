3f931a75848e17aaadc6135005e4afc9bf3d7249
Jsquad <lakis006@umn.edu>
2020-06-22 23:11:22 -0500

    added the correct files

---
+++ employee.js
@@ -0,0 +1,23 @@
+const mysql = require('mysql');
+const inquirer = require("inquirer");
+
+let connection = mysql.createConnection({
+    host: "localhost",
+    port: 9000,
+    user: "root",
+    password: "#Jsquadisgr8",
+    database: "employee_trackerdb"
+});
+
+inquirer.prompt ({
+    type:
+    message: 
+    name:
+    choices: [
+
+    ]
+}).then(
+
+)
+
+inquirer.prompt
